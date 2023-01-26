5769b65d9ef73a50cbcaac83b4dcf3fc78395186
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-26 11:24:35 -0600

    Clase 25 Configuracion de CSS para React

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +26,15 @@ module.exports = {
                 use: [
                     {loader: 'html-loader'}
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    //'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -32,6 +42,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
         })
     ],
     devServer: {
