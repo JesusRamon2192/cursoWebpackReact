da3bd01aa92c2f8d1560c8610737c608438e5d5f
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-26 11:01:06 -0600

    Clase 24 Configuracion de plugins y loaders para react

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    mode: 'development',
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -17,14 +19,27 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {loader: 'html-loader'}
+                ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
         },
         compress: true,
         port: 3006,
+        open: true,
     }
 }
